7e722177c59044f3510753c7c3901a2dd0a15c21
muktadeer <muktadeer@gmail.com>
2022-09-14 00:04:19 +0600

    secu new korlam

--- src/main/java/com/myproject/test/myproject/config/SecurityConfig.java
+++ src/main/java/com/myproject/test/myproject/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.myproject.test.myproject.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -12,7 +16,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
+
         return new BCryptPasswordEncoder();
+
+    }
+
+    @Autowired
+    private LoggingAccessDeniedHandler loggingAccessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeRequests()
+                .antMatchers("/static/**","/dist/**"  ,"/css/**","/fonts/**","/js/**","/images/**","/ie8-panel/**").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/user_login").permitAll()
+                .successForwardUrl("/loginsuccess")
+                .and().logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll()
+                .and().exceptionHandling().accessDeniedHandler(loggingAccessDeniedHandler);
+
     }
 
 
